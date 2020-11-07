032177633e0b10d5e0a4e15286d072ce53ab2c41
kretika arora <kretikaarora@gmail.com>
2020-11-07 20:24:35 +0530

    [Kretika Arora][Refractor]Adding Exceptions

---
+++ HotelManagementSystem/HotelReservationsCustomException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelManagementSystem
+{
+    class HotelReservationsCustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_CUSTOMERTYPE, INVALID_STAYDATE
+        }
+        public ExceptionType type;
+        public HotelReservationsCustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
